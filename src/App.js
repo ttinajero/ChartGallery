219d9338b30d308d6867e82a8adc51186082a717
Tavo Tinajero <tavo.tinajero@gmail.com>
2022-02-05 12:59:21 -0600

    load

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chart from './components/Chart';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+   // this.getchartData(); // this should be this.getChartData();
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['México', 'Guadalajara', 'Monterrey', 'Puebla', 'Cd Juarez', 'Veracruz'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Portafolio React Charts</h2>
+        </div>
+        <Chart chartData={this.state.chartData} location="México" legendPosition="bottom"/>
+      </div>
+    );
+  }
+}
+
+export default App;
